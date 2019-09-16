f29f89bc43b366a0dd4aef5134ad0a5d8411d14c
12three <zakharenko.main@gmail.com>
2019-09-16 18:37:42 +0300

    handle static files

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const app = express();
 
 app.set('view engine', 'pug');
 
+app.use(express.static('public'));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
